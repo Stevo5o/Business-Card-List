1eeed4992dff7b0083cf98a0e1efe83e6dbb22e3
Stephen <Stephen@O-Connor-ToshL>
2015-01-22 18:07:34 +0000

    click === user.id 6

--- _/myscript.js
+++ _/myscript.js
@@ -10,21 +10,31 @@
       success: function ( data ) {
          var output = "";
          $.each( data, function ( key, val ) {
+
             for ( var i = 0; i < val.length; i++ ) {
-               var user = val[i];
-               console.log( val[i] );
-               output += "<li><a id='pop' href='#" + user.id + "'>" + user.username + "</a></li>";
+               user = val[i];
+               console.log( user.id );
+               output += "<li><a id='" + user.id + "' href='#" + user.id + "'>" + user.username + "</a></li>";
             }
             $( 'ul#user_list' ).append( output );
 
-            // this is in the wrong place
-            $( "a#pop[href]" ).click( function () {
-               var myWindow = window.open( "", "", "width=400, height=200" );
-               myWindow.document.write( "Username: ", user.name + "<br />" +
-                       "Job Title: ", user.jobTitle + "<br />" +
-                       "Email Address: ", user.emailAdd );
-               myWindow.document.title = "Users";
-            } );
+            document.getElementById( 'user_list' ).addEventListener( 'click', function ( e )
+            {
+               console.log( e.toElement );
+               if ( e.toElement.id === user.id )
+               {
+                  console.log( 'yes' );
+                  var myWindow = window.open( "", "", "width=400, height=200" );
+                  myWindow.document.write( "Username: ", user.name + "<br />" +
+                          "Job Title: ", user.jobTitle + "<br />" +
+                          "Email Address: ", user.emailAdd );
+                  myWindow.document.title = "Users";
+               }
+               else
+               {
+                  console.log( 'no' );
+               }
+            }, false );
          } );
       }
    } );
@@ -32,12 +42,12 @@
    //  sort data in ascending and descending order
    $( document ).ready( function () {
       $( '.link-sort-list' ).click( function ( e ) {
-         var $sort = this;
-         var $list = $( '#user_list' );
-         var $listLi = $( 'li', $list );
+         var $sort = this,
+                 $list = $( '#user_list' ),
+                 $listLi = $( 'li', $list );
          $listLi.sort( function ( a, b ) {
-            var keyA = $( a ).text();
-            var keyB = $( b ).text();
+            var keyA = $( a ).text(),
+                    keyB = $( b ).text();
             if ( $( $sort ).hasClass( 'asc' ) )
             {
                return ( keyA > keyB ) ? 1 : 0;
