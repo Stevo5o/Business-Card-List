5273a26c97826740f861a68535efd60bff12fa4c
Stephen <Stephen@O-Connor-ToshL>
2015-01-22 08:30:33 +0000

    minify jQuery add comments to myscript

--- _/myscript.js
+++ _/myscript.js
@@ -1,8 +1,7 @@
 /* @ Stephen O'Connor */
 ( function () {
 
-   var userIndex;
-
+   // jQuery AJAX call
    $.ajax( {
       url: '_/data.json',
       dataType: 'json',
@@ -17,6 +16,8 @@
                output += "<li><a id='pop' href='#" + user.id + "'>" + user.username + "</a></li>";
             }
             $( 'ul#user_list' ).append( output );
+
+            // this is in the wrong place
             $( "a#pop[href]" ).click( function () {
                var myWindow = window.open( "", "", "width=400, height=200" );
                myWindow.document.write( "Username: ", user.name + "<br />" +
@@ -27,25 +28,28 @@
          } );
       }
    } );
-$(document).ready(function() {
-    $('.link-sort-list').click(function(e) {
-        var $sort = this;
-        var $list = $('#user_list');
-        var $listLi = $('li',$list);
-        $listLi.sort(function(a, b){
-            var keyA = $(a).text();
-            var keyB = $(b).text();
-            if($($sort).hasClass('asc')){
-                return (keyA > keyB) ? 1 : 0;
+
+   //  sort data in ascending and descending order
+   $( document ).ready( function () {
+      $( '.link-sort-list' ).click( function ( e ) {
+         var $sort = this;
+         var $list = $( '#user_list' );
+         var $listLi = $( 'li', $list );
+         $listLi.sort( function ( a, b ) {
+            var keyA = $( a ).text();
+            var keyB = $( b ).text();
+            if ( $( $sort ).hasClass( 'asc' ) )
+            {
+               return ( keyA > keyB ) ? 1 : 0;
             } else {
-                return (keyA < keyB) ? 1 : 0;
+               return ( keyA < keyB ) ? 1 : 0;
             }
-        });
-        $.each($listLi, function(index, row){
-            $list.append(row);
-        });
-        e.preventDefault();
-    });
-});
+         } );
+         $.each( $listLi, function ( index, row ) {
+            $list.append( row );
+         } );
+         e.preventDefault();
+      } );
+   } );
 }() );
 
